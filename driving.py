3efefcf97421833a42263463f2cc833d1dc97f7d
icebear <icebearliangliang@163.com>
2019-04-12 16:20:35 +0800

    add driving

---
+++ driving.py
@@ -0,0 +1,13 @@
+country = input('please input your country: ')
+age = input('please input your age: ')
+age = int(age)
+if country == 'China':
+	if age >= 18:
+		print('you can get a driving license')
+	else:
+		print("you can't get a driving license yet")
+elif country == 'American':
+	if age >= 16:
+		print('you can get a driving license')
+	else:
+		print("you can't get a driving license yet")
